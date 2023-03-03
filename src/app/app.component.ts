2cea4e225e0a2ec42cdfd7e323ab87357655d9f6
ahmedkouraichi2 <kouraichiahmed2@gmail.com>
2023-03-03 10:43:15 +0100

    liste de pokémon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    
+  constructor(){
+
+  }
+  ngOnInit(): void {
+     console.table(this.pokemonList);
+     this.selectPokemon(this.pokemonList[0]);
+  }
+  pokemonList = ['Bulbizarre','Salaméche','Carapuce']
   title = 'projetAngularPokemeon';
+
+
+        selectPokemon(pokemonName : string){
+        console.log(`vous aves cliquer sur le pokumon n ${pokemonName}`);
+      }
+
 }
